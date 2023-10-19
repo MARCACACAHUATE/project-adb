3b68177a7cfaae409152e1ea8bf812f8f33c971b
MARCACACAHUATE <marcacacahuate@outlook.com>
2023-10-18 23:36:29 -0500

    fix: Change sessions app name to sessiones

--- project_adb/settings.py
+++ project_adb/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # custom apps
-    'usuarios'
+    'usuarios',
+    'practicas',
+    'grupos',
+    'sessiones'
 ]
 
 MIDDLEWARE = [
--- usuarios/models.py
+++ usuarios/models.py
@@ -36,7 +36,7 @@ class Role(models.Model):
 
 
 class Registro_Semestre(models.Model):
-    alumno_id = models.ForeignKey("Usuario", null=True, on_delete=models.CASCADE)
+    alumno_id = models.ForeignKey("Usuarios", null=True, on_delete=models.CASCADE)
     is_valid = models.BooleanField(default=True, null=False)
     fecha_inicio = models.DateTimeField(null=False)
     fecha_final = models.DateTimeField(null=False)
