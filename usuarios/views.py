1efb86dd203ede9526a05133523979d794afdaa6
juancatap <juantapmtz02@gmail.com>
2023-10-21 11:45:24 -0600

    login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 
-#from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-from usuarios.forms import UserCreationForm
+from usuarios.forms import UserCreationForm, UserChangeForm
+from usuarios.models import Usuarios
 
 def home(request):
 
@@ -11,18 +13,46 @@ def home(request):
 #signup
 def registro(request):
 
+    form = UserCreationForm()
+
     if request.method == 'POST':
         
+        #password_confirm = request.POST['password_confirm']
+        
+        if UserChangeForm.is_valid(request):
 
-        print(request.POST)
-        print('obteniendo datos')
+            usuarios = Usuarios()
 
-    else: 
-        print('enviando formulario')
+            usuarios.role_id = form.cleaned_data['role_id']
+            usuarios.nombre = form.cleaned_data['nombre']
+            usuarios.matricula = form.cleaned_data['matricula']
+            usuarios.correo = form.cleaned_data['correo']
+            usuarios.password = form.cleaned_data['password']
 
-    return render(request, "registro.html", {
-        "form": UserCreationForm
-        })
+        #if password==password_confirm:
+         #   if Usuarios.objects.filter(matricula=matricula).exists():
+          #      messages.info(request, 'Username is already taken')
+           #     return redirect(registro)
+            #elif Usuarios.objects.filter(correo=correo).exists():
+             #   messages.info(request, 'Email is already taken')
+              #  return redirect(registro)
+
+        
+        #user = Usuarios.objects.create_user(role_id=role_id, matricula=matricula, password=password, correo=correo, nombre=nombre)
+            usuarios.save()
+                
+        else:
+            print("Invalido")
+
+    
+    return render(request, 'registro.html')
+
+
+
+    
+#, {
+        #"form": UserCreationForm
+        #})
     
 
 
