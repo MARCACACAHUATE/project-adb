58b53895ea2c18bc719dbc70affa6f58183b66ca
juancatap <juantapmtz02@gmail.com>
2023-11-03 09:48:47 -0600

    correccion de error

--- practicas/views/__init__.py
+++ practicas/views/__init__.py
@@ -1,6 +1,6 @@
 from .crear_practicas import CreatePracticasView
-<<<<<<< HEAD
+
 from .upload_Act import UploadActView
-=======
+
 #from .crear_practicascopy import CreatePracticasView2
->>>>>>> d5a5bac50e72bb917c8de92d0cfb1964f0df5dee
+
