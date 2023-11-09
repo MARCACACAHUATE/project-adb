a7d5c435226ff11055fcd2cee5e270f1db823322
MARCACACAHUATE <marcacacahuate@outlook.com>
2023-11-09 03:52:59 -0600

    feat: Pantalla de detalles para las practicas

--- practicas/urls.py
+++ practicas/urls.py
@@ -4,7 +4,8 @@ from practicas.views import (
     CreatePracticasView, 
     UploadActView, 
     activate_practicas, 
-    ListaPracticasView
+    ListaPracticasView,
+    DetailPracticasView
 )
 
 app_name = 'practicas'
@@ -12,6 +13,7 @@ app_name = 'practicas'
 urlpatterns = [
     path('grupo/<int:grupo_id>', CreatePracticasView.as_view(), name='crear'),
     path("grupo/<int:grupo_id>/list", ListaPracticasView.as_view(), name="list"),
+    path("grupo/<int:grupo_id>/list/<int:practica_id>", DetailPracticasView.as_view(), name="detail"),
     # no las he revisado
     path('subir_practicas/', UploadActView.as_view(), name='upload_Act'),
     path('ver/', UploadActView.as_view(), name='upload_Act'),
--- practicas/views/__init__.py
+++ practicas/views/__init__.py
@@ -1,3 +1,4 @@
 from .crear_practicas import CreatePracticasView
 from .upload_Act import UploadActView
 from .listaPracticas import ListaPracticasView
+from .detail_practicas import DetailPracticasView
