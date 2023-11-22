2d300d8ca204e40a8ea8c470b3cc52c3cdc1cea2
MARCACACAHUATE <marcacacahuate@outlook.com>
2023-11-22 16:21:12 -0600

    fix: fix7 add roles a la db

--- usuarios/management/commands/iniciar_user.py
+++ usuarios/management/commands/iniciar_user.py
@@ -1,4 +1,4 @@
-from usuarios.models import Usuarios
+from usuarios.models import Usuarios, Role
 from django.core.management.base import BaseCommand
 from decouple import config
 
@@ -9,6 +9,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         matricula = config("MATRICULA")
         password = config("PASSWORD")
+        Role.objects.create(Role="Admin")
+        Role.objects.create(Role="Maestro")
+        Role.objects.create(Role="Alumno")
 
         user = Usuarios.objects.create_superuser(matricula=matricula, password=password)
         self.stdout.write(f"Admin creado matricula {matricula} password {password}", self.style.SUCCESS)
