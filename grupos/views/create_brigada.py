ec65bd423db5ffc22cd8c65c151192b5ecff22de
MARCACACAHUATE <marcacacahuate@outlook.com>
2023-11-25 21:53:58 -0600

    feat: Validacion de las matriculas al cargar alumnos desde un csv

--- grupos/views/create_brigada.py
+++ grupos/views/create_brigada.py
@@ -26,12 +26,9 @@ class CreateBrigadaView(View):
         #    alumnos_siase = response.json()["alumnos"]
         #    print(alumnos_siase)
 
-        form = self.form_class()
-
-        print(grupo.alumnos.all())
         lista_alumnos = grupo.alumnos.all()
         return render(request, self.template_name, { 
-            "form": form,
+            "form": self.form_class(),
             "grupo": grupo,
             "lista_alumnos": lista_alumnos
         })
@@ -40,12 +37,6 @@ class CreateBrigadaView(View):
 
         grupo = Grupos.objects.get(pk=self.kwargs["grupo_id"])
 
-        #response = requests.get(f"http://127.0.0.1:36165/grupos/{grupo.numero_brigada}")
-
-        #alumnos_siase: dict = [] 
-        #if response.status_code == 200:
-        #    alumnos_siase = response.json()["alumnos"]
-        #    print(alumnos_siase)
 
         inicio_semestre = date.today()
         fin_semestre = inicio_semestre + relativedelta(months=+2)
@@ -58,15 +49,18 @@ class CreateBrigadaView(View):
             reader = csv.DictReader(io.StringIO(csv_file.read().decode('utf-8-sig')))
             alumno_role = Role.objects.get(Role="Alumno")
 
+            index = 1
             for row in reader:
 
                 try:
+                    # Validar los datos del csv
+                    self.validate_csv_data(row, index)
+                    
                     alumno, created = Usuarios.objects.get_or_create(
                         matricula=row["matricula"],
                         defaults= { "nombre": row["nombre"] }
                     )
 
-                    print(created)
                     if created:
                         alumno.role_id = alumno_role
                         Registro_Semestre.objects.create(
@@ -89,6 +83,19 @@ class CreateBrigadaView(View):
                         "grupo": grupo,
                         "lista_alumnos": lista_alumnos
                     })
+                
+                except ValueError as error:
+                    error_message = error
+                    lista_alumnos = grupo.alumnos.all()
+                    return render(request, self.template_name, {
+                        "error_message": error_message,
+                        "form": form,
+                        "grupo": grupo,
+                        "lista_alumnos": lista_alumnos
+                    })
+
+                index+=1
+
     
             return redirect(f"/grupos/{self.kwargs['grupo_id']}")
 
@@ -97,6 +104,23 @@ class CreateBrigadaView(View):
         
         return render(request, self.template_name)
 
-    def verify_alumnos(alumno):
-        #response = requests.get()
+    def verify_alumnos(self, alumno):
+
+        #response = requests.get(f"http://127.0.0.1:36165/grupos/{grupo.numero_brigada}")
+
+        #alumnos_siase: dict = [] 
+        #if response.status_code == 200:
+        #    alumnos_siase = response.json()["alumnos"]
+        #    print(alumnos_siase)
+
         print("validando alumnos")
+
+    def validate_csv_data(self, csv_data: dict[str, str], index: int):
+        # Valida tipo de dato correcto de la matricula
+        matricula = csv_data["matricula"]
+        if matricula.isdigit() is False:
+            raise ValueError(f"La matrícula '{matricula}' del renglón {index} no es valida.")
+        
+        # Valida el size maximo de la matricula
+        if len(matricula) > 10:
+            raise ValueError(f"La matrícula del renglon {index} debe ser de 10 digitos como máximo.")
